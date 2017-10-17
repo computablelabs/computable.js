ff6d27fd13d150489abf9de9e833746811dfd1da
Bharath Ramsundar <rbharath@stanford.edu>
2017-10-17 14:25:05 -0700

    Changes to make workflow example functional

--- examples/workflow.py
+++ examples/workflow.py
@@ -13,8 +13,8 @@ user_two_data = "GATTAACCAATTAGAGA"
 user_one_private_key = "*&^#*HWEF(#@*R#(@JF"
 user_two_private_key = ")(*#$)(*#)@#(&(@#JJLk"
 
-user_one_client = dm.data.Client(user_one_private_key)
-user_two_client = dm.data.Client(user_two_private_key)
+user_one_client = dm.data.InsecureFileClient(user_one_private_key)
+user_two_client = dm.data.InsecureFileClient(user_two_private_key)
 
 
 # The data_ledger objects contain records of where the data is stored.
@@ -28,4 +28,4 @@ user_one_retrieved_data = user_one_client.retrieve(user_one_data_ledger)
 user_two_retrieved_data = user_two_client.retrieve(user_two_data_ledger)
 
 assert user_one_data == user_one_retrieved_data
-assert user_two_data == user_one_retrieved_data
+assert user_two_data == user_two_retrieved_data
