ec71ac9d36f7f5f11716d9a3633d171ab1e6862d
Bharath Ramsundar <rbharath@stanford.edu>
2017-10-23 14:31:01 -0700

    Changes to make simple economy run

---
+++ datamined/coins/__init__.py
@@ -0,0 +1,9 @@
+"""
+Gathers sub imports here.
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+
+from datamined.coins.wallets import Wallet 
+from datamined.coins.wallets import ExampleWallet 
--- examples/compute_workflow.py
+++ examples/compute_workflow.py
@@ -6,11 +6,19 @@
 import datamined as dm
 
 data = "ATTAGGACATTTATA"
-
-validator = dm.valid.NaiveGenomicValidator()
-
 private_key = "*&^#*HWEF(#@*R#(@JF"
-client = dm.data.InsecureFileClient(private_key)
+
+# Create a data validator.
+validator = dm.valid.NaiveGenomicValidator()
+# Check whether data is valid
+assert validator.is_valid(data)
+
+# Create an introductory client wallet 
+client_wallet = dm.coins.ExampleWallet()
+assert client_wallet.token_count() == 0
+
+client = dm.data.InsecureFileClient(private_key, client_wallet)
+assert client.get_wallet().token_count() == 0
 ledger = client.store(data, validator)
 
 # Assumes that dm.data.Client objects have the ability to generate
@@ -22,10 +30,16 @@ ledger = client.store(data, validator)
 # partial access keys that can (ideally) be revoked?
 ledger_key = client.get_ledger_key(ledger)
 
-node = dm.compute.GenomicCountInsecureFileNode()
+# Create an introductory node wallet 
+node_wallet = dm.coins.ExampleWallet()
+assert node_wallet.token_count() == 0
+
+node = dm.compute.GenomicCountInsecureFileNode(node_wallet)
 # It's still an open question how a Node can perform arbitrary computations. In
 # this very early stage, going to punt and assume nodes can only perform a
 # limited set of computations linked to by keywords.
 results = node.compute(ledger, ledger_key, "count-basepairs")
+# The node should have been paid for this computation
+assert node.get_wallet().token_count() > 0
 
 assert results == {"A": 6, "T": 6, "G": 2, "C": 1}
