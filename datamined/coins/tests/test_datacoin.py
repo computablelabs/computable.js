318bc157af3dbc3435bfaaeea86bbe84883aeecf
Bharath Ramsundar <rbharath@stanford.edu>
2017-11-04 22:08:45 -0700

    Moved some tests over

--- datamined/coins/tests/test_datacoin.py
+++ datamined/coins/tests/test_datacoin.py
@@ -7,8 +7,11 @@ from __future__ import unicode_literals
 
 import unittest
 import web3
+import datetime
+from web3 import Web3
 import datamined as dm
 from populus import Project
+from ethereum import tester as t
 from datamined.coins.wallets import DATACOIN_PATH
 
 
@@ -37,6 +40,40 @@ class TestDataCoin(unittest.TestCase):
       # Token contract address is not yet set.
       assert crowdsale.call().tokenReward(
       ) == '0x0000000000000000000000000000000000000000'
+
+  def test_initialized(self):
+    """Crowdsale is properly initialized with given parameters."""
+
+    project_dir = DATACOIN_PATH
+    project = Project(project_dir, create_config_file=True)
+    with project.get_chain('tester') as chain:
+      beneficiary = chain.web3.eth.accounts[3]
+      multisig = chain.web3.eth.accounts[4]
+      
+      # Initialize crowdsale
+      args = [beneficiary, multisig, 0]
+      crowdsale, _ = chain.provider.get_or_deploy_contract(
+          'Crowdsale', deploy_args=args)
+
+      # Initialize token
+      args = [beneficiary]  # Owner set
+      token, _ = chain.provider.get_or_deploy_contract(
+          'DataCoin', deploy_args=args)
+      assert crowdsale.call().tokenReward(
+      ) == '0x0000000000000000000000000000000000000000'
+      crowdsale.transact({"from": beneficiary}).setToken(
+          token.address)
+      assert crowdsale.call().tokenReward(
+      ) != '0x0000000000000000000000000000000000000000'
+
+      token.transact({"from": beneficiary}).approve(
+          crowdsale.address, 440000000)
+
+      assert token.call().balanceOf(beneficiary) == 500000000
+      assert token.call().totalSupply() == 500000000
+      assert token.call().owner().lower() == beneficiary
+      assert token.call().allowance(beneficiary, crowdsale.address) == 440000000
+      assert token.call().owner().lower() == crowdsale.call().beneficiary().lower()
 
   def test_token_initialized(self):
     """Crowdsale is properly initialized with given parameters."""
@@ -105,3 +142,245 @@ class TestDataCoin(unittest.TestCase):
       # Post last deadline prcie
       crowdsale.transact().setCurrent(deadlines[-1] + 1)
       assert crowdsale.call().getPrice() == 1000000000000000
+
+  def test_get_price_tiers_2(self):
+    """Price tiers match given dates."""
+    project_dir = DATACOIN_PATH
+    project = Project(project_dir, create_config_file=True)
+    with project.get_chain('tester') as chain:
+      beneficiary = chain.web3.eth.accounts[3]
+      multisig = chain.web3.eth.accounts[4]
+      customer = chain.web3.eth.accounts[1]
+      
+      # Initialize crowdsale
+      args = [beneficiary, multisig, 0]
+      crowdsale, _ = chain.provider.get_or_deploy_contract(
+          'Crowdsale', deploy_args=args)
+
+      # Initialize token
+      args = [beneficiary]  # Owner set
+      token, _ = chain.provider.get_or_deploy_contract(
+          'DataCoin', deploy_args=args)
+      assert crowdsale.call().tokenReward(
+      ) == '0x0000000000000000000000000000000000000000'
+      crowdsale.transact({"from": beneficiary}).setToken(
+          token.address)
+      assert crowdsale.call().tokenReward(
+      ) != '0x0000000000000000000000000000000000000000'
+
+      token.transact({"from": beneficiary}).approve(
+          crowdsale.address, 440000000)
+
+      deadlines = [1488297600, 1488902400, 1489507200, 1490112000]
+      prices = [833333333333333, 909090909090909, 952380952380952, 1000000000000000]
+
+      for idx, deadline in enumerate(deadlines):
+        crowdsale.transact().setCurrent(deadline - 1)
+        assert crowdsale.call().getPrice() == prices[idx]
+
+      # Post last deadline prcie
+      crowdsale.transact().setCurrent(deadlines[-1] + 1)
+      assert crowdsale.call().getPrice() == 1000000000000000
+
+  def test_dates(self):
+    """Dates match given in the project material."""
+    project_dir = DATACOIN_PATH
+    project = Project(project_dir, create_config_file=True)
+    with project.get_chain('tester') as chain:
+      beneficiary = chain.web3.eth.accounts[3]
+      multisig = chain.web3.eth.accounts[4]
+      customer = chain.web3.eth.accounts[1]
+      
+      # Initialize crowdsale
+      args = [beneficiary, multisig, 0]
+      crowdsale, _ = chain.provider.get_or_deploy_contract(
+          'Crowdsale', deploy_args=args)
+
+      # Initialize token
+      args = [beneficiary]  # Owner set
+      token, _ = chain.provider.get_or_deploy_contract(
+          'DataCoin', deploy_args=args)
+      assert crowdsale.call().tokenReward(
+      ) == '0x0000000000000000000000000000000000000000'
+      crowdsale.transact({"from": beneficiary}).setToken(
+          token.address)
+      assert crowdsale.call().tokenReward(
+      ) != '0x0000000000000000000000000000000000000000'
+
+      token.transact({"from": beneficiary}).approve(
+          crowdsale.address, 440000000)
+
+      deadlines = [
+          crowdsale.call().deadlines(0),
+          crowdsale.call().deadlines(1),
+          crowdsale.call().deadlines(2),
+          crowdsale.call().deadlines(3)
+      ]
+
+      print("Start is {}".format(
+          datetime.datetime.fromtimestamp(
+              crowdsale.call().start(), tz=datetime.timezone.utc)))
+
+      for idx, deadline in enumerate(deadlines):
+        print("Deadline {} is {}".format(idx,
+                                        datetime.datetime.fromtimestamp(
+                                            deadline, tz=datetime.timezone.utc)))
+
+      print("Token is transferable {}".format(
+          datetime.datetime.fromtimestamp(
+              token.call().startTime(), tz=datetime.timezone.utc)))
+
+      assert token.call().startTime() == deadlines[-1]
+
+#@pytest.fixture
+#def start():
+#  """Match in TestableCrowdsale."""
+#  return 1488294000
+#
+#
+#@pytest.fixture
+#def end():
+#  """Match in TestableCrowdsale."""
+#  return 1490112000
+#
+#
+#@pytest.fixture
+#def open_crowdsale(crowdsale, token, start):
+#  """We live in time when crowdsale is open"""
+#  crowdsale.transact().setCurrent(start + 1)
+#  token.transact().setCurrent(start + 1)
+#  return crowdsale
+
+  #def test_buy_tokens(open_crowdsale, token, customer, beneficiary, web3):
+#  def test_buy_tokens(self):
+#    """Sending ETH successfully buys tokens."""
+#    project_dir = DATACOIN_PATH
+#    project = Project(project_dir, create_config_file=True)
+#    chain_name = "tester"
+#
+#    with project.get_chain(chain_name) as chain:
+#      # setup
+#      accounts = chain.web3.eth.accounts
+#      customer = accounts[1]
+#      beneficiary = accounts[3]
+#      multisig = accounts[4]
+#      # crowdsale
+#      crowdsale, _ = chain.provider.get_or_deploy_contract(
+#          'Crowdsale', deploy_args=[beneficiary, multisig, 0])
+#      # token
+#      token, _ = chain.provider.get_or_deploy_contract(
+#          'DataCoin', deploy_args=[beneficiary])
+#      crowdsale.transact({"from": beneficiary}).setToken(token.address)
+#      start = 1488294000
+#      crowdsale.transact().setCurrent(start + 1)
+#      token.transact().setCurrent(start + 1)
+#      open_crowdsale = crowdsale
+#
+#      print("Customer")
+#      balance = chain.web3.eth.getBalance(customer)
+#      print("wei balance: ", balance)
+#      eth = Web3.fromWei(balance, "ether")
+#      print("eth balance: ", eth)
+#
+#      print("Open Crowdsale")
+#      balance = chain.web3.eth.getBalance(open_crowdsale.address)
+#      print("wei balance: ", balance)
+#      eth = Web3.fromWei(balance, "ether")
+#      print("eth balance: ", eth)
+#      print("ATTEMPTS")
+#      #gas_estimate = web3.utils.transactions.get_buffered_gas_estimate(chain.web3, {
+#      #    "from": customer,
+#      #    "to": open_crowdsale.address,
+#      #    "value": Web3.toWei(20, "ether"),
+#      #})
+#      #print("gas_estimate: ", gas_estimate)
+#      #gas_estimate = chain.web3.eth.estimateGas({
+#      #    "from": customer,
+#      #    "to": open_crowdsale.address,
+#      #    "value": Web3.toWei(20, "ether"),
+#      #})
+#      #print("gas_estimate: ", gas_estimate)
+#      #assert 0 == 1
+#
+#      #t.gas_price = 0
+#      chain.web3.eth.sendTransaction({
+#          "from": customer,
+#          "to": open_crowdsale.address,
+#          "value": Web3.toWei(20, "ether"),
+#          "gas": 3000000,
+#          "gasPrice": 1,
+#      })
+#
+#      # We get ERC-20 event
+#      events = token.pastEvents("Transfer").get()
+#      assert len(events) == 1
+#      e = events[0]
+#      assert e["args"]["to"].lower() == customer
+#      assert e["args"]["from"].lower() == beneficiary
+#      assert e["args"]["value"] == 24000
+#
+#      # We get crowdsale event
+#      events = open_crowdsale.pastEvents("FundTransfer").get()
+#      assert len(events) == 1
+#      e = events[0]
+#      assert e["args"]["backer"].lower() == customer
+#      assert e["args"]["amount"] == Web3.toWei(20, "ether")
+#      assert e["args"]["amountRaised"] == Web3.toWei(20, "ether")
+#
+
+  def test_buy_tokens(self):
+    """Sending ETH successfully buys tokens."""
+    start = 1488294000
+    end = 1490112000
+    project_dir = DATACOIN_PATH
+    project = Project(project_dir, create_config_file=True)
+    with project.get_chain('tester') as chain:
+      beneficiary = chain.web3.eth.accounts[3]
+      multisig = chain.web3.eth.accounts[4]
+      customer = chain.web3.eth.accounts[1]
+      
+      # Initialize crowdsale
+      args = [beneficiary, multisig, 0]
+      crowdsale, _ = chain.provider.get_or_deploy_contract(
+          'Crowdsale', deploy_args=args)
+
+      # Initialize token
+      args = [beneficiary]  # Owner set
+      token, _ = chain.provider.get_or_deploy_contract(
+          'DataCoin', deploy_args=args)
+      assert crowdsale.call().tokenReward(
+      ) == '0x0000000000000000000000000000000000000000'
+      crowdsale.transact({"from": beneficiary}).setToken(
+          token.address)
+      assert crowdsale.call().tokenReward(
+      ) != '0x0000000000000000000000000000000000000000'
+
+      token.transact({"from": beneficiary}).approve(
+          crowdsale.address, 440000000)
+
+      # Doing open crowdsale
+      crowdsale.transact().setCurrent(start + 1)
+      token.transact().setCurrent(start + 1)
+
+      chain.web3.eth.sendTransaction({
+          "from": customer,
+          "to": crowdsale.address,
+          "value": Web3.toWei(20, "ether"),
+          "gas": 250000,
+      })
+
+      # We get ERC-20 event
+      events = token.pastEvents("Transfer").get()
+      assert len(events) == 1
+      e = events[0]
+      assert e["args"]["to"].lower() == customer
+      assert e["args"]["from"].lower() == beneficiary
+      assert e["args"]["value"] == 24000
+
+      # We get crowdsale event
+      events = crowdsale.pastEvents("FundTransfer").get()
+      assert len(events) == 1
+      e = events[0]
+      assert e["args"]["backer"].lower() == customer
+      assert e["args"]["amount"] == Web3.toWei(20, "ether")
+      assert e["args"]["amountRaised"] == Web3.toWei(20, "ether")
--- examples/conftest.py
+++ examples/conftest.py
@@ -1,8 +1,54 @@
 """Test fixtures."""
 
+import os
 import pytest
 from web3.contract import Contract
+import sys
 
+from populus.project import Project
+from datamined.coins.wallets import DATACOIN_PATH
+
+def get_populus_option(cmdline_option, ini_option, environ_var, pytestconfig, default=None):
+
+    if pytestconfig.getoption(cmdline_option, default=False):
+            return pytestconfig.getoption(cmdline_option)
+    else:
+        if pytestconfig.getini(ini_option):
+            return pytestconfig.getini(ini_option)
+        elif environ_var in os.environ.keys():
+            return os.environ[environ_var]
+
+    return default
+
+
+
+@pytest.fixture()
+def web3(chain):
+    return chain.web3
+
+
+@pytest.fixture()
+#def project(request, pytestconfig):
+#def project(pytestconfig):
+def project():
+
+    #project_dir = get_populus_option(
+    #    cmdline_option="--populus-project",
+    #    ini_option="populus_project",
+    #    environ_var="PYTEST_POPULUS_PROJECT",
+    #    pytestconfig=pytestconfig,
+    #    default=pytestconfig.args[0]
+    #)
+    project_dir = DATACOIN_PATH
+
+    project = Project(project_dir, create_config_file=True)
+
+    return project
+
+@pytest.yield_fixture()
+def chain(project):
+    with project.get_chain('tester') as chain:
+        yield chain
 
 # http://stackoverflow.com/q/28898919/315168
 def pytest_itemcollected(item):
@@ -30,7 +76,7 @@ def token(chain, crowdsale, beneficiary):
   #owner = crowdsale.address
   args = [beneficiary]  # Owner set
   contract, _ = chain.provider.get_or_deploy_contract(
-      'EdgelessToken', deploy_args=args)
+      'DataCoin', deploy_args=args)
   assert crowdsale.call().tokenReward(
   ) == '0x0000000000000000000000000000000000000000'
   crowdsale.transact({"from": beneficiary}).setToken(contract.address)
@@ -136,3 +182,7 @@ def empty_address(accounts):
 def allowed_party(accounts):
   """Gets ERC-20 allowance."""
   return accounts[5]
+
+@pytest.fixture()
+def accounts(web3):
+    return web3.eth.accounts
