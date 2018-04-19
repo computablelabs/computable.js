ee6153ec78814f2d8d18ab2a01b1269a613305ab
robrobbins <rob.robb.ns@gmail.com>
2018-04-18 18:00:06 -0700

    Cleanup old files and introduce the new ones

--- old-py/datamined/compute/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Gathers sub imports here.
-"""
-from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-
-from datamined.compute.nodes import Node
-from datamined.compute.nodes import GenomicCountInsecureFileNode
--- old-py/datamined/valid/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Gathers sub imports here.
-"""
-from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-
-from datamined.valid.validators import Validator
-from datamined.valid.validators import NaiveGenomicValidator
