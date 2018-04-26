dbf8acd44b311a3fea58c13c64e5cf210289c833
Rob Robbins <rob.robb.ns@gmail.com>
2018-04-26 16:42:09 -0700

    moar parameterizer specs

--- spec/computable/parameterizer/re-param.spec.ts
+++ spec/computable/parameterizer/re-param.spec.ts
@@ -19,6 +19,7 @@ describe('Parameterizer', () => {
   beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
 
+    // paramaterizer needs a deployed token
     eip20 = await new web3.eth.Contract(tJson.abi, undefined, { gasPrice: 100, gas: 4500000 })
       .deploy({ data: tJson.bytecode, arguments: [
         Token.supply,
@@ -26,20 +27,24 @@ describe('Parameterizer', () => {
         Token.decimals,
         Token.symbol
       ]})
-      .send({ from: accounts[0], gasPrice: 100, gas: 4500000 }) // NOTE watch the gas limit here
+      .send({ from: accounts[0] }) // NOTE watch the gas limit here
 
     eip20.setProvider(provider)
     const tokenAddress = eip20.options.address
 
-    parameterizer = await new web3.eth.Contract(pJson.abi)
+    // use the deployed token address
+    parameterizer = await new web3.eth.Contract(pJson.abi, undefined, { gasPrice: 100, gas: 4500000 })
       .deploy({ data: pJson.bytecode, arguments: [
         tokenAddress,
         Addresses.Three, // TODO use deployed voting contract
         ...getDefaults()
       ]})
-      .send({ from: accounts[0], gasPrice: 100, gas: 4500000 })
+      .send({ from: accounts[0] })
 
     parameterizer.setProvider(provider)
+
+    // approve the parameterizer with the token, account[0] has all the balance atm
+    await eip20.methods.approve(parameterizer.options.address, 1000000).send({ from: accounts[0] })
   })
 
   // describe('Expected failures', () => {
@@ -58,9 +63,35 @@ describe('Parameterizer', () => {
     // })
   // })
 
+  it('parameterizer has an allowance of a million', async () => {
+    const allowance = await eip20.methods.allowance(accounts[0], parameterizer.options.address).call()
+    expect(parseInt(allowance)).toBe(1000000)
+  })
+
   it('returns falsy for non-existant proposal', async () => {
     // const pid = await parameterizer.methods.proposeReparameterization('voteQuorum', 51).send({ from: accounts[0], gasPrice: 100, gas: 4500000 })
     const res = await parameterizer.methods.propExists(web3.utils.asciiToHex('foo')).call()
     expect(res).toBe(false)
+  })
+
+  describe('Process proposal', () => {
+    it('adds a new proposal', async () => {
+      const applicantStartingBalance = await eip20.methods.balanceOf(accounts[0]).call()
+      expect(applicantStartingBalance).toBe('5000000')
+
+      const tx = await parameterizer.methods.proposeReparameterization('voteQuorum', 51).send({ from: accounts[0] })
+      expect(tx).toBeTruthy()
+      // propId is nested in the event TODO change to using the event listener when they work
+      const propID = tx.events._ReparameterizationProposal.returnValues.propID,
+        proposed = await parameterizer.methods.proposals(propID).call()
+      expect(proposed.name).toBe('voteQuorum')
+      expect(proposed.value).toBe('51')
+
+      // proposer should have been charged PMinDeposit
+      const deposit = await parameterizer.methods.get('pMinDeposit').call()
+      expect(deposit).toBe('100')
+      const applicantFinalBalance = await eip20.methods.balanceOf(accounts[0]).call()
+      expect(parseInt(applicantFinalBalance)).toBe((parseInt(applicantStartingBalance)) - (parseInt(deposit)))
+    })
   })
 })
