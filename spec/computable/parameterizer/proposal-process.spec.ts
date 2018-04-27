3abd661e7cce247b09e991a65b74d251b145bb39
Rob Robbins <rob.robb.ns@gmail.com>
2018-04-27 16:20:15 -0700

    processing proposals working, helpers for time manipulation added

--- spec/computable/parameterizer/proposal-process.spec.ts
+++ spec/computable/parameterizer/proposal-process.spec.ts
@@ -3,8 +3,8 @@ import Web3 from 'web3'
 import { Contract } from '../../../node_modules/web3/types.d'
 import pJson from '../../../computable/build/contracts/Parameterizer.json'
 import tJson from '../../../computable/build/contracts/EIP20.json'
-import { getDefaults } from './helpers'
-import { Addresses, Token } from '../../../src/constants'
+import { getDefaults, increaseTime } from './helpers'
+import { ParameterDefaults, Addresses, Token } from '../../../src/constants'
 import Bignumber from 'bignumber'
 
 const provider:any = ganache.provider(),
@@ -43,7 +43,28 @@ describe('Parameterizer: Reparamaterize', () => {
     parameterizer.setProvider(provider)
 
     // approve the parameterizer with the token, account[0] has all the balance atm
-    // await eip20.methods.approve(parameterizer.options.address, 1000000).send({ from: accounts[0] })
+    await eip20.methods.approve(parameterizer.options.address, 1000000).send({ from: accounts[0] })
+  })
+
+  it('can process a proposal', async () => {
+    const applicantStartingBalance = await eip20.methods.balanceOf(accounts[0]).call()
+    expect(applicantStartingBalance).toBe('5000000')
+
+    const tx = await parameterizer.methods.proposeReparameterization('voteQuorum', 51).send({ from: accounts[0] })
+    expect(tx).toBeTruthy()
+
+    const tx2 = await increaseTime(provider, ParameterDefaults.PApplyStageLength + 1)
+    expect(tx2).toBeTruthy()
+    // propId is nested in the event TODO change to using the event listener when they work
+    const propID = tx.events._ReparameterizationProposal.returnValues.propID,
+      tx3 = await parameterizer.methods.processProposal(propID).send({ from: accounts[0] })
+    expect(tx3).toBeTruthy()
+    // we should see the changes now
+    const vq = await parameterizer.methods.get('voteQuorum').call()
+    expect(vq).toBe('51')
+    // the proposer should have had their tokens returned
+    const applicantFinalBalance = await eip20.methods.balanceOf(accounts[0]).call()
+    expect(applicantStartingBalance).toBe(applicantFinalBalance)
   })
 
 })
