416ace62deb645114d5ac961f90e60b92109302e
Rob Robbins <rob.robb.ns@gmail.com>
2018-04-30 13:39:44 -0700

    Actually commit the changed file. I read that helps...

--- spec/computable/parameterizer/helpers.ts
+++ spec/computable/parameterizer/helpers.ts
@@ -1,23 +1,5 @@
 import { ParameterDefaults } from '../../../src/constants'
 
-// the paramaterizer takes a massive number of args, here are some defaults for it
-export function getDefaults(): any[] {
-  return [
-    ParameterDefaults.MinDeposit,
-    ParameterDefaults.PMinDeposit,
-    ParameterDefaults.ApplyStageLength,
-    ParameterDefaults.PApplyStageLength,
-    ParameterDefaults.CommitStageLength,
-    ParameterDefaults.PCommitStageLength,
-    ParameterDefaults.RevealStageLength,
-    ParameterDefaults.PRevealStageLength,
-    ParameterDefaults.DispensationPct,
-    ParameterDefaults.PDispensationPct,
-    ParameterDefaults.VoteQuorum,
-    ParameterDefaults.PVoteQuorum
-  ]
-}
-
 // NOTE the web3 typed IProvider is incomplete (no sendAsync etc...) TODO flush it out
 export function increaseTime(provider: any, seconds: number): Promise<any> {
   return new Promise((resolve, reject) =>
