aa81f045906f182ed3e3b759dcbcdb7c6fadb216
Rob Robbins <rob.robb.ns@gmail.com>
2018-05-21 18:20:50 -0700

    update registry and more specs for it

--- spec/computable/registry/app-was-made.spec.ts
+++ spec/computable/registry/app-was-made.spec.ts
@@ -1,17 +1,13 @@
 import * as ganache from 'ganache-cli'
 import Web3 from 'web3'
 import { Contract } from '../../../node_modules/web3/types.d'
-import { ParameterDefaults, NAME } from '../../../src/constants'
-import {
-  eventReturnValues,
-  deployDll,
-  deployAttributeStore,
-} from '../../../src/helpers'
-import { stringToBytes } from '../../helpers'
+import { deployDll, deployAttributeStore } from '../../../src/helpers'
+import { stringToBytes, increaseTime } from '../../helpers'
 import Eip20 from '../../../src/contracts/eip-20'
 import Voting from '../../../src/contracts/plcr-voting'
 import Parameterizer from '../../../src/contracts/parameterizer'
 import Registry from '../../../src/contracts/registry'
+import { ParameterDefaults, NAME } from '../../../src/constants'
 
 const provider:any = ganache.provider(),
   web3 = new Web3(provider)
@@ -24,7 +20,7 @@ let accounts:string[],
   parameterizer:Parameterizer,
   registry:Registry
 
-describe('PLCRVoting', () => {
+describe('Registry', () => {
   beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
 
@@ -56,50 +52,49 @@ describe('PLCRVoting', () => {
     await eip20.approve(votingAddress, 1000000)
     await eip20.approve(registryAddress, 1000000)
     // 1st account, as challenger, needs funds
-    await eip20.transfer(accounts[1], 500000)
+    // await eip20.transfer(accounts[1], 500000)
     // 2nd account as voter needs funds
-    await eip20.transfer(accounts[2], 500000)
+    // await eip20.transfer(accounts[2], 500000)
     // registry needs to be approved to spend ond the challenger's behalf
-    await eip20.approve(registryAddress, 450000, { from: accounts[1] })
+    // await eip20.approve(registryAddress, 450000, { from: accounts[1] })
     // voting needs approval from voter
-    await eip20.approve(votingAddress, 450000, { from: accounts[2] })
+    // await eip20.approve(votingAddress, 450000, { from: accounts[2] })
   })
 
-  it('has deployed', () => {
-    expect(voting).toBeTruthy()
-    expect(voting.getAddress()).toBeTruthy()
-  })
+  it('returns true if application expiry was previously initialized', async () => {
+    const listBytes = stringToBytes(web3, 'listing.com'),
+      tx1 = await registry.apply(listBytes, ParameterDefaults.MIN_DEPOSIT) // apply
 
-  it('commits vote, updates DLL state', async () => {
-    const domainOne = stringToBytes(web3, 'one.net'),
-      domainTwo = stringToBytes(web3, 'two.net'),
-      tx1 = await registry.apply(domainOne, ParameterDefaults.MIN_DEPOSIT),
-      tx2 = await registry.apply(domainTwo, ParameterDefaults.MIN_DEPOSIT)
+    let result = await registry.appWasMade(listBytes)
 
-    expect(tx1).toBeTruthy()
+    expect(result).toBe(true)
+
+    // commit stage complete
+    await increaseTime(provider, ParameterDefaults.COMMIT_STAGE_LENGTH + 1)
+    result = await registry.appWasMade(listBytes)
+    expect(result).toBe(true) // still true because not expired
+
+    // reveal stage complete, update status (whitelist it)
+    await increaseTime(provider, ParameterDefaults.REVEAL_STAGE_LENGTH + 1)
+    const tx2 = await registry.updateStatus(listBytes)
     expect(tx2).toBeTruthy()
 
-    const challID1 = eventReturnValues('_Challenge',
-      await registry.challenge(domainOne, '', { from: accounts[1] }), 'challengeID')
-    expect(challID1).toBeTruthy()
+    const isWhitelisted = await registry.isWhitelisted(listBytes)
+    expect(isWhitelisted).toBe(true)
 
+    result = await registry.appWasMade(listBytes)
+    expect(result).toBe(true) // still true since was whitelisted
 
-    const challID2 = eventReturnValues('_Challenge',
-      await registry.challenge(domainTwo, '', { from: accounts[1] }), 'challengeID')
-    expect(challID2).toBeTruthy()
-
-    // we'll let the defaults get used for vote, tokens and salt
-    const tx5 = await voting.commitVote(web3, challID1, accounts[2], 1, 10, 420)
-    expect(tx5).toBeTruthy()
-    // bump the num tokens here...
-    const tx6 = await voting.commitVote(web3, challID2, accounts[2], 1, 11, 420)
-    expect(tx6).toBeTruthy()
-    // insert point for less than the default (10) should then be 0
-    const pt = await voting.getInsertPointForNumTokens(accounts[2], 9, challID1)
-    expect(pt).toBe('0')
-    // converse should be true for more...
-    const pt2 = await voting.getInsertPointForNumTokens(accounts[2], 12, challID2)
-    expect(pt2).toBe('1')
+    // exit
+    const tx3 = await registry.exit(listBytes)
+    expect(tx3).toBeTruthy()
+    result = await registry.appWasMade(listBytes) // should be false now as exit was called
+    expect(result).toBe(false)
   })
 
+  it('returns falsy if applicationExpiry was not initialized', async () => {
+    const listBytes = stringToBytes(web3, 'nope.com')
+
+    expect(await registry.appWasMade(listBytes)).toBe(false)
+  })
 })
--- src/contracts/registry.ts
+++ src/contracts/registry.ts
@@ -15,10 +15,10 @@ import {
  * Shape of the parameter object that should be passed to this class during a deploy.
  */
 interface RegistryDeployParams {
-  tokenAddress: string;
-  votingAddress: string;
-  parameterizerAddress: string;
-  name: string;
+  tokenAddress: string; // deployed address of the token that this contract references
+  votingAddress: string; // deployed address of a PLCRVoting contract this contract references
+  parameterizerAddress: string; // deployed address of a Parameterizer this contract references
+  name: string; // The name of this registry
 }
 
 /**
@@ -30,6 +30,8 @@ interface RegistryDeployParams {
  *
  * Getters:
  * -------
+ * appWasMade
+ * isWhiteListed
  * name
  * parameterizer
  * token
@@ -49,6 +51,15 @@ export default class extends Deployable {
       account = this.requireAccount(opts)
 
     return await deployed.methods.apply(listing, tokens, data).send({ from: account })
+  }
+
+  /**
+   * Returns a bool that indicates if `apply` was called for a given listing hash
+   */
+  async appWasMade(listing:string): Promise<boolean> {
+    const deployed = this.requireDeployed()
+
+    return deployed.methods.appWasMade(listing).call()
   }
 
   /**
@@ -85,6 +96,27 @@ export default class extends Deployable {
   }
 
   /**
+   * Allows the owner of a listingHash to remove the listingHash from the whitelist
+   * Returns all tokens to the owner of the listingHash
+   * @param listing listing that msg.sender is the owner of
+   */
+  async exit(listing:string, opts?:ContractOptions): Promise<TransactionReceipt> {
+    const deployed = this.requireDeployed(),
+      account = this.requireAccount(opts)
+
+    return await deployed.methods.exit(listing).send({ from: account })
+  }
+
+  /**
+   * Return a bool indicating if this listing has been whitelisted
+   */
+  async isWhitelisted(listing:string): Promise<boolean> {
+    const deployed = this.requireDeployed()
+
+    return await deployed.methods.isWhitelisted(listing).call()
+  }
+
+  /**
    * Return the name passed to this contract instance at deploy time
    */
   async name(): Promise<string> {
@@ -109,6 +141,17 @@ export default class extends Deployable {
     const deployed = this.requireDeployed()
 
     return await deployed.methods.token().call()
+  }
+
+  /**
+   * Updates a listingHash's status from 'application' to 'listing' or resolves a challenge if one exists.
+   * Delegates to `whitelistApplication` or `resolveChallenge`
+   */
+  async updateStatus(listing:string, opts?:ContractOptions): Promise<TransactionReceipt> {
+    const account = this.requireAccount(opts),
+      deployed = this.requireDeployed()
+
+    return await deployed.methods.updateStatus(listing).send({ from: account })
   }
 
   /**
