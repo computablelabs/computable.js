6834f0aa52c02e4848cd95c2328fa2637e019487
Geoff Kinder <geoff.kinder@gmail.com>
2018-09-12 09:14:49 -0700

    Rebuilt module.

--- dist/contracts/plcr-voting.d.ts
+++ dist/contracts/plcr-voting.d.ts
@@ -5,13 +5,13 @@ import { Nos } from '../types';
 import { ContractOptions, VotingDeployParams, AtParams } from '../interfaces';
 export default class  extends Deployable {
     at(web3: Web3, params: AtParams, opts?: ContractOptions): Promise<boolean>;
-    commitVote(web3: Web3, pollID: string, voter: string, vote: Nos, tokens: Nos, salt: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
+    commitVote(web3: Web3, id: Nos, voter: string, vote: Nos, tokens: Nos, salt: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
     deploy(web3: Web3, params: VotingDeployParams, opts?: ContractOptions): Promise<string>;
-    getInsertPointForNumTokens(voter: string, tokens: Nos, pollID: string): Promise<Nos>;
+    getInsertPointForNumTokens(voter: string, tokens: Nos, id: Nos): Promise<Nos>;
     getVoteSaltHash(web3: Web3, vote: Nos, salt: Nos): string;
     requestVotingRights(tokens: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
     withdrawVotingRights(tokens: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
-    revealVote(pollID: Nos, vote: Nos, salt: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
-    commitPeriodActive(pollID: Nos): Promise<boolean>;
-    revealPeriodActive(pollID: Nos): Promise<boolean>;
+    revealVote(id: Nos, vote: Nos, salt: Nos, opts?: ContractOptions): Promise<TransactionReceipt>;
+    commitPeriodActive(id: Nos): Promise<boolean>;
+    revealPeriodActive(id: Nos): Promise<boolean>;
 }
