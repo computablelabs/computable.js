d26dc9a681ade176590901547fa560821721be8b
robrobbins <rob.robb.ns@gmail.com>
2019-07-02 16:12:51 -0700

    finish erc20 and ethertokens

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ https://github.com/computablelabs/contracts contains the smart contract code for
 ### Setup
 * git clone...
 * `npm install`
-* `git submodule init` (first time after clone only)
-* `git submodule update --remote`
 * `npm run test`
 
+#### Note on the compiled contract files
+If there is ever a need to manually update the compiled `abi` and `bin` files clone the [computable main repo]() as a sibling to this one
+and use the package.json `update` scripts.
+
+These files are not included in the Typescript build, as a browser environment can't make use of them. The `abi` interface for each contract
+is stored as a constant, and used to instantiate each *Higher Order Contract* as well as to look up gas cost on demand. Bytecode files,
+_.bin_, are only required for specs thus can be read utilizing the Node.js `readfile` based helper `readBytecode` (`.bin` files are not included
+in the Typescript build)
---
+++ spec/base.spec.ts
@@ -0,0 +1,59 @@
+import * as ganache from 'ganache-cli'
+import Web3 from 'web3'
+import { Contract } from 'web3/types'
+import EtherToken from '../src/contracts/ether-token'
+import { ETHER_TOKEN_ABI, ONE_ETHER, ONE_GWEI } from  '../src/constants'
+import { Return } from '../src/@types'
+import {
+  deploy,
+  readBytecode,
+  call,
+  transact,
+} from '../src/helpers'
+
+const provider:any = ganache.provider(),
+  w3 = new Web3(provider, undefined, {defaultBlock: 'latest',
+    transactionConfirmationBlocks: 1, transactionBlockTimeout: 5}),
+    toBN = w3.utils.toBN
+
+// we'll use the ether token contract to test the deployed base methods here (could be any contract)
+let base:EtherToken,
+  accounts:string[],
+  deployed:Contract
+
+describe('Deployed Base Class', () => {
+  beforeAll(async () => {
+    accounts = await w3.eth.getAccounts()
+
+    // deploy it...
+    const bin:string = readBytecode('ethertoken')
+
+    deployed = await deploy(w3, accounts[0], ETHER_TOKEN_ABI,
+      bin, [accounts[0], ONE_ETHER])
+
+    // now we can instantiate the HOC
+    base = new EtherToken(accounts[0])
+    await base.at(w3, deployed.options.address)
+  })
+
+  describe('Super class methods for all HOCs', () => {
+    it('assigns defaults to an empty object', () => {
+      const defaults = base.assignTransactOpts({})
+      expect(defaults.from).toBe(accounts[0])
+      expect(defaults.gasPrice).toBeTruthy()
+    })
+
+    it('will use passed in values when assigning transact opts', () => {
+      // see github.com/indutny/bn.js for all available BN methods
+      let oneGwei = toBN(ONE_GWEI)
+      const defaults = base.assignTransactOpts({value: oneGwei.mul(toBN(3)).toString()})
+      expect(defaults.from).toBe(accounts[0])
+      expect(defaults.value).toBe(oneGwei.mul(toBN(3)).toString())
+    })
+
+    it('will overwrite from the opts', () => {
+      const defaults = base.assignTransactOpts({to: '0xfoo'}, {to: '0xbar'})
+      expect(defaults.to).toBe('0xbar')
+    })
+  })
+})
