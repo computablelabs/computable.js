c2ba4c3794a95af7762f10a118a0a45f752fbbbd
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-07-09 14:14:33 +0800

    Parameterizer HOC's (#150)
    * Parameterizer HOC's
    
    * update gas expects
    
    * use real addresses and real parameter values

--- spec/parameterizer/parameterizer.spec.ts
+++ spec/parameterizer/parameterizer.spec.ts
@@ -1,31 +1,330 @@
 import * as ganache from 'ganache-cli'
 import Web3 from 'web3'
+import { Contract } from 'web3/types'
 import Parameterizer from '../../src/contracts/parameterizer'
-import { maybeParseInt } from '../../src/helpers'
+import { PARAMETERIZER_ABI, ONE_ETHER, ONE_GWEI } from  '../../src/constants'
+import { Return } from '../../src/@types'
+import {
+  deploy,
+  readBytecode,
+  call,
+  transact,
+} from '../../src/helpers'
 
 const provider:any = ganache.provider(),
-  w3 = new Web3(provider)
+  w3 = new Web3(provider, undefined, {defaultBlock: 'latest',
+    transactionConfirmationBlocks: 1, transactionBlockTimeout: 5}),
+    toBN = w3.utils.toBN
 
-let accounts:string[],
-  parameterizer:Parameterizer
+const marketTokenAddress = "0x931D387731bBbC988B312206c74F77D004D6B84b"
+
+const priceFloor = w3.utils.toWei('1', 'szabo')
+const spread = 110
+const listReward = w3.utils.toWei('250', 'szabo')
+const stake = w3.utils.toWei('10', 'finney')
+const voteBy = 100
+const plurality = 50
+const backendPayment = 25
+const makerPayment = 25
+const costPerByte = w3.utils.toWei('100', 'gwei')
+
+let parameterizer:Parameterizer,
+  accounts:string[],
+  deployed:Contract
 
 describe('Parameterizer', () => {
-  beforeAll(() => {
-
-  })
-
-  afterAll(() => {
-
-  })
-
-  beforeEach(async () => {
+  beforeAll(async () => {
     accounts = await w3.eth.getAccounts()
 
-    // parameterizer = new Parameterizer(accounts[0])
+    // deploy it...
+    const bin:string = readBytecode('voting')
+
+    deployed = await deploy(w3,
+                            accounts[0],
+                            PARAMETERIZER_ABI,
+                            bin,
+                            [marketTokenAddress,
+                              priceFloor,
+                              spread,
+                              listReward,
+                              stake,
+                              voteBy,
+                              plurality,
+                              backendPayment,
+                              makerPayment,
+                              costPerByte])
+
+    // now we can instantiate the HOC
+    parameterizer = new Parameterizer(accounts[0])
+    await parameterizer.at(w3, deployed.options.address)
   })
 
+  describe('Class methods for Parameterizer', () => {
 
-  it('has been instantiated from an existing deployment', async () => {
+    it('calls getBackendPayment correctly', async () => {
+      const defaults = await parameterizer.getBackendPayment({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getBackendPayment')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
 
+    it('calls getMakerPayment correctly', async () => {
+      const defaults = await parameterizer.getMakerPayment({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getMakerPayment')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getReservePayment correctly', async () => {
+      const defaults = await parameterizer.getReservePayment({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getReservePayment')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getCostPerByte correctly', async () => {
+      const defaults = await parameterizer.getCostPerByte({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getCostPerByte')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getStake correctly', async () => {
+      const defaults = await parameterizer.getStake({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getStake')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getPriceFloor correctly', async () => {
+      const defaults = await parameterizer.getPriceFloor({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getPriceFloor')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getHash correctly', async () => {
+      const defaults = await parameterizer.getHash(2, 42, {})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getHash')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getSpread correctly', async () => {
+      const defaults = await parameterizer.getSpread({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getSpread')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getListReward correctly', async () => {
+      const defaults = await parameterizer.getListReward({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getListReward')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getPlurality correctly', async () => {
+      const defaults = await parameterizer.getPlurality({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getPlurality')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getReparam correctly', async () => {
+      const defaults = await parameterizer.getReparam('hash', {})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getReparam')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls getVoteBy correctly', async () => {
+      const defaults = await parameterizer.getVoteBy({})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('getVoteBy')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls reparameterize correctly', async () => {
+      const defaults = await parameterizer.reparameterize(2, 42, {})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('reparameterize')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
+
+    it('calls resolveReparam correctly', async () => {
+      const defaults = await parameterizer.resolveReparam('hash',{})
+      let tx = defaults[0]
+      let opts = defaults[1]
+      let gas = parameterizer.getGas('resolveReparam')
+      expect(tx).not.toBeNull(accounts[0])
+      expect(opts).not.toBeNull()
+      expect(Object.keys(opts).indexOf('gas')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('from')).toBeGreaterThan(-1)
+      expect(Object.keys(opts).indexOf('gasPrice')).toBeGreaterThan(-1)
+      expect(opts['gas']).toBeGreaterThanOrEqual(gas)
+      let from = opts['from']
+      expect(from).not.toBeNull()
+      expect(from!.trim().length).toBeGreaterThan(0)
+      let gasPrice = opts['gasPrice']
+      expect(gasPrice).not.toBeNull()
+      expect(parseInt(gasPrice)).toBeGreaterThan(0)
+    })
   })
 })
--- src/contracts/parameterizer.ts
+++ src/contracts/parameterizer.ts
@@ -1,11 +1,9 @@
 import Web3 from 'web3'
+import {  } from 'web3/types'
+import { TransactOpts } from '../interfaces'
 import { PARAMETERIZER_ABI } from '../constants'
+import { Return } from '../@types'
 import Deployed from '../abstracts/deployed'
-import { Nos } from '../@types'
-import {
-  TransactOpts,
-  Reparam,
-} from '../interfaces'
 
 export default class extends Deployed {
   /**
@@ -14,5 +12,89 @@ export default class extends Deployed {
    */
   at(w3:Web3, address:string, opts?:TransactOpts): Promise<boolean> {
     return super.at(w3, address, PARAMETERIZER_ABI, opts)
+  }
+
+  async getBackendPayment(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getBackendPayment')}, opts)
+    return [await deployed.methods.getBackendPayment(), assigned]
+  }
+
+  async getMakerPayment(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getMakerPayment')}, opts)
+    return [await deployed.methods.getMakerPayment(), assigned]
+  }
+
+  async getReservePayment(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getReservePayment')}, opts)
+    return [await deployed.methods.getReservePayment(), assigned]
+  }
+
+  async getCostPerByte(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getCostPerByte')}, opts)
+    return [await deployed.methods.getCostPerByte(), assigned]
+  }
+
+  async getStake(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getStake')}, opts)
+    return [await deployed.methods.getStake(), assigned]
+  }
+
+  async getPriceFloor(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getPriceFloor')}, opts)
+    return [await deployed.methods.getPriceFloor(), assigned]
+  }
+
+  async getHash(param:Number, value:Number, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getHash')}, opts)
+    return [await deployed.methods.getHash(param, value), assigned]
+  }
+
+  async getSpread(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getSpread')}, opts)
+    return [await deployed.methods.getSpread(), assigned]
+  }
+
+  async getListReward(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getListReward')}, opts)
+    return [await deployed.methods.getListReward(), assigned]
+  }
+
+  async getPlurality(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getPlurality')}, opts)
+    return [await deployed.methods.getPlurality(), assigned]
+  }
+
+  async getReparam(hash:string, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getReparam')}, opts)
+    return [await deployed.methods.getReparam(hash), assigned]
+  }
+
+  async getVoteBy(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getVoteBy')}, opts)
+    return [await deployed.methods.getVoteBy(), assigned]
+  }
+
+  async reparameterize(param:Number, value:Number, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('reparameterize')}, opts)
+    return [await deployed.methods.reparameterize(param, value), assigned]
+  }
+
+  async resolveReparam(hash:string, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('resolveReparam')}, opts)
+    return [await deployed.methods.resolveReparam(hash), assigned]
   }
 }
