f4c4f24fb29bed559dd9fe0b489640cf675fb704
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-07-09 14:08:55 +0800

    Reserve HOC's (#149)
    * Reserve HOC's
    
    * update gas expects
    
    * use real addresses in spec

--- src/contracts/reserve.ts
+++ src/contracts/reserve.ts
@@ -1,7 +1,9 @@
 import Web3 from 'web3'
-import { RESERVE_ABI } from '../constants'
-import Deployed from '../abstracts/deployed'
+import {  } from 'web3/types'
 import { TransactOpts } from '../interfaces'
+import { RESERVE_ABI } from '../constants'
+import { Return } from '../@types'
+import Deployed from '../abstracts/deployed'
 
 export default class extends Deployed {
   /**
@@ -10,5 +12,29 @@ export default class extends Deployed {
    */
   at(w3:Web3, address:string, opts?:TransactOpts): Promise<boolean> {
     return super.at(w3, address, RESERVE_ABI, opts)
+  }
+
+  async getSupportPrice(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getSupportPrice')}, opts)
+    return [await deployed.methods.getSupportPrice(), assigned]
+  }
+
+  async support(offer:string, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('support')}, opts)
+    return [await deployed.methods.support(offer), assigned]
+  }
+
+  async getWithdrawalProceeds(address:string, opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('getWithdrawalProceeds')}, opts)
+    return [await deployed.methods.getWithdrawalProceeds(address), assigned]
+  }
+
+  async withdraw(opts?:TransactOpts): Promise<Return> {
+    const deployed = this.requireDeployed()
+    let assigned = this.assignTransactOpts({gas: this.getGas('withdraw')}, opts)
+    return [await deployed.methods.withdraw(), assigned]
   }
 }
